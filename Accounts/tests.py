a2b73965b7e895382bfa14c175fa4d6aec00ba30
BERCHIL Idriss <16048625+Jonahgay@users.noreply.github.com>
2023-05-19 17:15:23 +0100

    add test case + update view

--- Accounts/tests.py
+++ Accounts/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class Test_Account_views(TestCase):
+
+    def test_Register_API(self):
+        Register_Path = reverse("Account_Register")
+        Responce = self.client.get("/")
+
+        csrf =  str(Responce.cookies).split(";")[0].split("=")[1]
+
+        Responce = self.client.post(
+            Register_Path, 
+            data={"Email": "Test@Test.com", "Password":"ABCD"}, 
+            headers={ "X-CSRFToken":csrf},
+            content_type="application/json"
+        )
+
+        self.assertTrue(Responce.status_code == 201)
+
+        Responce = self.client.post(
+            Register_Path, 
+            data={"Email": "Test@Test.com", "Password":"ABCD"},
+            content_type="application/json"
+            )
+        self.assertTrue(Responce.status_code == 409)
+
+        Responce = self.client.post(
+            Register_Path, 
+            data={"Email": "TestTest.com", "Password":"ABCD"},
+            content_type="application/json"
+            )
+
+        self.assertTrue(Responce.status_code == 400)
+
+        Responce = self.client.get(Register_Path)
+        self.assertTrue(Responce.status_code == 405)
--- Accounts/views.py
+++ Accounts/views.py
@@ -3,27 +3,32 @@ from django.http import HttpRequest, JsonResponse, HttpResponse
 from json import loads
 from re import match
 from django.contrib.auth.models import User
-#from django.contrib.auth import 
 
 # Create your views here.
+
 def Register_API(request:HttpRequest):
-    if not request.user.is_authenticated:
-        return redirect("")
+    if request.user.is_authenticated:
+        return JsonResponce({"message":"Is authenticated"},status=401)
     if not request.method == "POST":
-        return HttpResponse({"message":"Request is not using POST Method."},status_code=405)
+        return HttpResponse({"message":"Request is not using POST Method."},status=405)
     
     HTTP_Data = loads(request.body)
 
-    if not "Email" in HTTP_Data and not "Password" in HTTP_Data or len(HTTP_Data.keys()) != 2:
-        return JsonResponse({"message":"Json data must contain Email and Password keys."},status_code=400)
+    if not "Email" in HTTP_Data or not "Password" in HTTP_Data or len(HTTP_Data.keys()) != 2:
+        return HttpResponse({"message":"Json data must contain Email and Password keys."},status=400)
 
-    if bool(match(r"^\S@\S.[a-zA-Z]+", HTTP_Data["Email"])):
-        return JsonResponse({"message":"Email is not well formated."},status_code=400)
+    if not bool(match(r"^\S+@\S+.[a-zA-Z]+", HTTP_Data["Email"])):
+        return HttpResponse({"message":"Email is not well formated."},status=400)
 
-    if User.objects.get(username=HTTP_Data["Email"]) is None:
-        return JsonResponse({"message":"Email already exists."},status_code=409)
+    if User.objects.filter(username=HTTP_Data["Email"]).exists():
+        return HttpResponse({"message":"Email already exists."},status=409)
 
     User(username=HTTP_Data["Email"], password=HTTP_Data["Password"]).save()
-    returnJsonResponse({"message":"Done."},status_code=201)
+    return JsonResponse({"message":"Created."},status=201)
 
+def Login_API(request:HttpRequest):
+    pass
+
+def Account_Profile_API(request:HttpRequest):
+    pass
     
