5e384996fb8d8454a6e60a79801f64bca092b798
BERCHIL Idriss <16048625+Jonahgay@users.noreply.github.com>
2023-05-25 22:59:52 +0100

    Modify_Medical_form_API + Get_Medical_form_API

--- Accounts/views.py
+++ Accounts/views.py
@@ -17,13 +17,13 @@ def Register_API(request:HttpRequest):
     HTTP_Data = loads(request.body)
 
     if not "Email" in HTTP_Data or not "Password" in HTTP_Data or len(HTTP_Data.keys()) != 2:
-        return HttpResponse({"message":"Json data must contain Email and Password keys."},status=400)
+        return JsonResponse({"message":"Json data must contain Email and Password keys."},status=400)
 
     if not bool(match(r"^\S+@\S+.[a-zA-Z]+", HTTP_Data["Email"])):
-        return HttpResponse({"message":"Email is not well formated."},status=400)
+        return JsonResponse({"message":"Email is not well formated."},status=400)
 
     if User.objects.filter(username=HTTP_Data["Email"]).exists():
-        return HttpResponse({"message":"Email already exists."},status=409)
+        return JsonResponse({"message":"Email already exists."},status=409)
 
     U = User(username=HTTP_Data["Email"], is_active=True)
     U.set_password(HTTP_Data["Password"])
@@ -81,7 +81,7 @@ def Get_Medical_form_API(request:HttpRequest):
                 "Height" : M.Heigth,
                 "Weight" : M.Weight,
                 "Blood_type" : M.Blood_type,
-                "Allergies" : [i.Name for i in M.all()[0].Allergies.all()]
+                "Allergies" : [i.Name for i in M.Allergies.all()]
             },status=200)
         else:
             return JsonResponse({"message":"You do not have a Medical_Form."}, status=404)
@@ -96,10 +96,62 @@ def Get_Medical_form_API(request:HttpRequest):
             "Height" : M.Heigth,
             "Weight" : M.Weight,
             "Blood_type" : M.Blood_type,
-            "Allergies" : [i.Name for i in M.all()[0].Allergies.all()]
+            "Allergies" : [i.Name for i in M.Allergies.all()]
         }, status=200)
     else:
         return JsonResponse({"message": "User does not have Group"}, status=401)
 
 def Modify_Medical_form_API(request:HttpRequest):
-    pass
+    if not request.user.is_authenticated:
+        return JsonResponse({"message":"User is not authenticated."},status=401)
+    if not request.method == "POST":
+        return JsonResponse({"message":"Request is not using POST Method."},status=405)
+    HTTP_data = loads(request.body)
+    
+    if request.user.groups.filter(name="Patient").exists():
+        if Medical_Form.objects.filter(Patient = request.user).exists():
+            if "Email" in HTTP_data:
+                if HTTP_data["Email"] is not None:
+                    return JsonResponse({"message": "Patients aren't allowed to enter Email"}, status=400)
+            M=Medical_Form.objects.filter(Patient = request.user)
+            if "Heigth" in HTTP_data:
+                if HTTP_data["Heigth"] is not None:
+                    M.Heigth = HTTP_data["Heigth"]
+            if "Weigth" in HTTP_data:
+                if HTTP_data["Weigth"] is not None:
+                    M.Weigth = HTTP_data["Weigth"]
+            if "Blood_type" in HTTP_data:
+                if HTTP_data["Blood_type"] is not None:
+                    M.Blood_type = HTTP_data["Blood_type"]
+            if "Blood_type" in HTTP_data:
+                if HTTP_data["Blood_type"] is not None:
+                    M.Allergies.clear()
+                    M.Allergies.add(*HTTP_data["Allergies"])
+            return JsonResponse({"message" : "Done."}, status=200)
+        else:
+            return JsonResponse({"message":"You do not have a Medical_Form."}, status=404)
+    elif request.user.groups.filter(name="Nurse").exists():
+
+        if HTTP_data.get("Email", None) is None:
+            return JsonResponse({"message": "Nurses should enter patient Email"}, status=400)
+
+        if not Medical_Form.objects.filter(Patient = User.objects.filter(username = HTTP_data["Email"])).exists():
+            return JsonResponse({"message":"Medical form not found in Patient."}, status=404)
+
+        M = Medical_Form.objects.filter(Patient = User.objects.filter(username = HTTP_data["Email"])).all()[0]
+        if "Heigth" in HTTP_data:
+            if HTTP_data["Heigth"] is not None:
+                M.Heigth = HTTP_data["Heigth"]
+        if "Weigth" in HTTP_data:
+            if HTTP_data["Weigth"] is not None:
+                M.Weigth = HTTP_data["Weigth"]
+        if "Blood_type" in HTTP_data:
+            if HTTP_data["Blood_type"] is not None:
+                M.Blood_type = HTTP_data["Blood_type"]
+        if "Blood_type" in HTTP_data:
+            if HTTP_data["Blood_type"] is not None:
+                M.Allergies.clear()
+                M.Allergies.add(*HTTP_data["Allergies"])
+        return JsonResponse({"message" : "Done."}, status=200)
+    else:
+        return JsonResponse({"message": "User does not have Group"}, status=401)
